851f86fedbec0a1397f22d1a689538c62445a61e
Brett Mostert <brettmostert@Bretts-MacBook-Pro.local>
2023-10-17 05:39:31 +0200

    ylo

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,34 +13,7 @@ return {
       { 'neovim/nvim-lspconfig' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-vsnip'},
-      { 'hrsh7th/vim-vsnip'},
-      --{
-      --   'L3MON4D3/LuaSnip',
-     --    version = "v2.*",                    -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-         -- install jsregexp (optional!).
-     --    build = "make install_jsregexp"
-     -- },
-      --{ 'saadparwaiz1/cmp_luasnip' }
-
+      { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/vim-vsnip' },
    }
-   --{
-   -- 'williamboman/mason.nvim',
-   -- opts = {
-   --    ensure_installed = {
-   --       "gopls",
-   --    }
-   -- },
-   -- dependencies = {
-   --    { 'williamboman/mason-lspconfig.nvim' },
-   -- { 'hrsh7th/cmp-nvim-lsp' },
-   -- { 'hrsh7th/nvim-cmp' },
-   -- { 'L3MON4D3/LuaSnip' },
-   -- }
-   --},
-   --{ 'neovim/nvim-lspconfig',
-   --config = function()
-   --   require "plugins.config.lspconfig"
-   --end,
-   --},
 }
