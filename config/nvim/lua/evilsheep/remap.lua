026dbc4cef76b0b01600334c83ecf66a0cda61bf
Brett Mostert <brettmostert@gmail.com>
2023-11-18 13:47:59 +0200

    removed unused key binding

--- config/nvim/lua/evilsheep/remap.lua
+++ config/nvim/lua/evilsheep/remap.lua
@@ -1,66 +1,29 @@
 vim.g.mapleader = " "
---vim.keymap.set("n", "<leader>pv", vim.cmd.E)
-
---vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
---vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
---vim.keymap.set("n", "J", "mzJ`z")
---vim.keymap.set("n", "<C-d>", "<C-d>zz")
---vim.keymap.set("n", "<C-u>", "<C-u>zz")
---vim.keymap.set("n", "n", "nzzzv")
---vim.keymap.set("n", "N", "Nzzzv")
-
--- vim.keymap.set("n", "<leader>vwm", function()
---    require("vim-with-me").StartVimWithMe()
---    end)
--- vim.keymap.set("n", "<leader>svwm", function()
---    require("vim-with-me").StopVimWithMe()
--- end)
-
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
---vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
---vim.keymap.set("n", "<leader>Y", [["+Y]])
-
---vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Trouble
 vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
 
--- This is going to get me cancelled
---vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<C-as>", "<cmd>silent !tmux new tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-
-
---vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/evilsheep/packer.lua<CR>");
--- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("n", "<leader>bl", ":Telescope buffers<CR>")
 
-vim.keymap.set("n","<leader>bl",":Telescope buffers<CR>")
-vim.keymap.set("n","<leader>b.",":bnext<CR>")
-vim.keymap.set("n","<leader>b,",":bprev<CR>")
+vim.keymap.set("n", "<leader>b.", ":bnext<CR>")
+vim.keymap.set("n", "<leader>b,", ":bprev<CR>")
 vim.keymap.set("n", "<leader>.", ":tabn<CR>")
 vim.keymap.set("n", "<leader>,", ":tabp<CR>")
-vim.keymap.set("n","<leader>nt",":tabnew<CR>")
+vim.keymap.set("n", "<leader>nt", ":tabnew<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+   vim.cmd("so")
 end)
