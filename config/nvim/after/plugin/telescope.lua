ce811cee38d6d2ee8fb53792670ba531005fdb1d
Brett Mostert <brettmostert@gmail.com>
2023-11-19 10:23:30 +0200

    updated key bindings

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,33 @@
-local builtin = require('telescope.builtin') 
+require("telescope").setup {
+  pickers = {
+    -- Your special builtin config goes in here
+    buffers = {
+      sort_lastused = true,
+      -- theme = "dropdown",
+      -- previewer = false,
+      mappings = {
+        i = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+        },
+        n = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+        }
+      }
+    },
+    -- find_files = {
+    --    -- theme = ""
+    -- }
+  },
+  extensions = {
+    -- Your extension config goes in here
+  }
+}
+
+
+local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });	
 end)
+
