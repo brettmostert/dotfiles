f1a6e10feafbb0544cec20f4a65890dbe0ad125e
Brett Mostert <brettmostert@gmail.com>
2024-07-29 17:57:13 +0200

    updated alacritty shortcuts

--- config/nvim/after/plugin/nvimtree.lua
+++ config/nvim/after/plugin/nvimtree.lua
@@ -1,4 +1,5 @@
 --vim.g.lf_netrw = 1
 
-vim.keymap.set("n", "<leader>ee", "<Cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>ee", "<Cmd>NvimTreeOpen<CR>")
 vim.keymap.set("n","<leader>ef","<Cmd>NvimTreeFindFile<CR>")
+vim.keymap.set("n","<leader>et","<Cmd>NvimTreeFinderToggle<CR>")
