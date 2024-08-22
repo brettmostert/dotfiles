d10e38b33ec2b20656da2e79d84183c1af0f4122
Brett Mostert <brett@stitch.money>
2024-08-22 22:59:14 +0200

    added aerospace

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -26,7 +26,8 @@ require("telescope").setup {
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pgit', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });	
 end)
