6cc00b5352af98566b425b279b91badd5feee137
Brett Mostert <brettmostert@gmail.com>
2024-10-13 10:36:36 +0200

    added keepawake raycast script

---
+++ config/raycast/scripts/raycastkeepingyouawake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title RayCastKeepingYouAwake
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName RayCastCustomScripts
+
+# Documentation:
+# @raycast.author Brett Mostert
+
+open keepingyouawake:///toggle --args shell=True
